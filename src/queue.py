939f1ed039c7eb502002faf559d0ceec1d3158ed
icarrera <irismcarrera@gmail.com>
2016-03-10 14:10:31 -0800

    Added dequeue, peak, and size methods on Dequeue. Created tests for those methods.

--- src/queue.py
+++ src/queue.py
@@ -14,3 +14,21 @@ class Queue(object):
     def enqueue(self, val):
         """Adds a value to the end of the queue."""
         self._container.append(val)
+
+    def dequeue(self):
+        """Removes and returns value from front of queue."""
+        temp_value = self._container.pop()
+        if temp_value:
+            return temp_value
+        raise ValueError
+
+    def peek(self):
+        """Returns the next value in the queue without dequeing it."""
+        try:
+            return self._container.head.val
+        except AttributeError:
+            return None
+
+    def size(self):
+        """Returns the size of the queue."""
+        return self._container.size()
--- src/test_queue.py
+++ src/test_queue.py
@@ -15,15 +15,26 @@ def test_Queue():
 
 
 def test_init_Queue(test_empty_Queue, test_Queue):
-    """Testing the __init__ method of Queue."""
     assert isinstance(test_empty_Queue, Queue)
     assert isinstance(test_Queue, Queue)
     assert test_Queue._container.to_string() == "(1, 2, 3)"
 
 
 def test_enqueue(test_empty_Queue, test_Queue):
-    """Tests the enqueue method of Queue."""
     test_Queue.enqueue(4)
     test_empty_Queue.enqueue(2)
     assert test_Queue._container.to_string() == "(1, 2, 3, 4)"
     assert test_empty_Queue._container.to_string() == "(2)"
+
+def test_dequeue(test_empty_Queue, test_Queue):
+    assert test_Queue.dequeue() == 1
+    with pytest.raises(ValueError):
+        test_empty_Queue.dequeue()
+
+def test_peek(test_empty_Queue, test_Queue):
+    assert test_Queue.peek() == 1
+    assert test_empty_Queue.peek() is None
+
+def test_size(test_empty_Queue, test_Queue):
+    assert test_Queue.size() == 3
+    assert test_empty_Queue.size() == 0
