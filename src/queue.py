0398675c8f897ffa18af12fa16044e3ab9a6b684
icarrera <irismcarrera@gmail.com>
2016-03-10 13:44:52 -0800

    added enqueue method that adds a value to the end of the queue and created a test for enqueue.

--- src/queue.py
+++ src/queue.py
@@ -4,8 +4,13 @@ from dll import DLL
 
 
 class Queue(object):
+    """Queue class that contains a DLL."""
     def __init__(self, iter=None):
         self._container = DLL()
         if iter:
             for val in iter:
                 self._container.append(val)
+
+    def enqueue(self, val):
+        """Adds a value to the end of the queue."""
+        self._container.append(val)
--- src/test_queue.py
+++ src/test_queue.py
@@ -15,7 +15,15 @@ def test_Queue():
 
 
 def test_init_Queue(test_empty_Queue, test_Queue):
-    """testing the __init__ method of Queue."""
+    """Testing the __init__ method of Queue."""
     assert isinstance(test_empty_Queue, Queue)
     assert isinstance(test_Queue, Queue)
     assert test_Queue._container.to_string() == "(1, 2, 3)"
+
+
+def test_enqueue(test_empty_Queue, test_Queue):
+    """Tests the enqueue method of Queue."""
+    test_Queue.enqueue(4)
+    test_empty_Queue.enqueue(2)
+    assert test_Queue._container.to_string() == "(1, 2, 3, 4)"
+    assert test_empty_Queue._container.to_string() == "(2)"
