5422c1dcef1ad996848ca26bfd6766232c435fed
icarrera <irismcarrera@gmail.com>
2016-03-11 20:16:37 -0800

    set up test variables for proper_parens function

---
+++ src/proper_parenthetics.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8-*-
+
+def proper_parens(uni_string):
+    """Determines the parens of a string are open, balanced, or broken."""
+    pass
---
+++ src/test_proper_parenthetics.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+import pytest
+from proper_paranthetics import proper_parens
+
+open_string_1 = u"(this string is open"
+
+balanced_string_1 = u"(this string is balanced)"
+
+broken_string_1 = u"this string is broken)"
+
+def test_proper_parens():
+    pass
