0c1d8533abc9831f469ef62c33ca0983617d4f0e
icarrera <irismcarrera@gmail.com>
2016-03-14 01:28:23 -0700

    created function that returns 0 if parens are balanced, 1 if they are open, and -1 if it is broken.

--- src/proper_parenthetics.py
+++ src/proper_parenthetics.py
@@ -1,5 +1,18 @@
 # -*- coding: utf-8-*-
+"""Whiteboard challenge - Proper Parentheses"""
 
-def proper_parens(uni_string):
-    """Determines the parens of a string are open, balanced, or broken."""
-    pass
+def proper_parens(text):
+    """Finds if the parentheses in a string are open, balanced, or broken."""
+    paren_val = 0
+    for char in text:
+        if char == "(":
+            paren_val += 1
+        elif char == ")":
+            paren_val -= 1
+            if paren_val < 0:
+                break
+    if paren_val > 0:
+        return 1
+    elif paren_val < 0:
+        return -1
+    return paren_val
