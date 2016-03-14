748a6ce083920a13f7cc6975a20d8a857e38861e
icarrera <irismcarrera@gmail.com>
2016-03-14 01:27:23 -0700

    created tests for open, balanced, and broken parentheses created strings to test against

--- src/test_proper_parenthetics.py
+++ src/test_proper_parenthetics.py
@@ -1,12 +1,21 @@
 # -*- coding: utf-8 -*-
 import pytest
-from proper_paranthetics import proper_parens
+from proper_parenthetics import proper_parens
 
-open_string_1 = u"(this string is open"
 
-balanced_string_1 = u"(this string is balanced)"
+open_1 = u"(((open)"
+balanced_1 = u"balanced(())"
+broken_1 = u"broken)"
+broken_2 = u")))((("
 
-broken_string_1 = u"this string is broken)"
+def test_open():
+    assert proper_parens(open_1) == 1
 
-def test_proper_parens():
-    pass
+def test_balanced():
+    assert proper_parens(balanced_1) == 0
+
+def test_broken_1():
+    assert proper_parens(broken_1) == -1
+
+def test_broken_2():
+    assert proper_parens(broken_2) == -1
