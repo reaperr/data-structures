489e4f2de5cd468b52e1dea032bfaafa088e38ec
Kyle R <KyleRichVA@gmail.com>
2016-03-15 18:41:18 -0700

    Added and tested pop() using new _head_check_down helper method. changed old heap_check to _heap_check_up to make different.

--- src/test_binheap.py
+++ src/test_binheap.py
@@ -8,7 +8,6 @@ def binheap():
     return BinaryHeap([34, 25, 10, 11, 3])
 
 
-
 def test_BinHeap_init():
     test_heap = BinaryHeap([1, 2, 3])
     assert test_heap._child_index(0) == (1, 2)
@@ -17,11 +16,20 @@ def test_BinHeap_init():
 
 def test_push(binheap):
     binheap.push(13)
-    assert binheap._heap_list[2] == 13
+    assert binheap.heap_list[2] == 13
 
 
-def test_heap_check(binheap):
-    binheap._heap_check(1, 3)
+def test_pop(binheap):
+    binheap.pop()
+    assert binheap.heap_list[3] == 3
+
+
+def test_heap_check_up(binheap):
+    binheap._heap_check_up(1, 3)
+
+
+def test_heap_check_down(binheap):
+    binheap._heap_check_down(0)
 
 
 def test_child_index(binheap):
