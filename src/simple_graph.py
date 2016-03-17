f562590aa70e60c0190cb206fcd269cab0e6ee8c
icarrera <irismcarrera@gmail.com>
2016-03-17 13:31:49 -0700

    began SimpleGraph object contructor.

---
+++ src/simple_graph.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 *-*
+
+
+class SimpleGraph(object):
+    """Python implementation of the simple graph structure."""
+
+    def __init__(self):
+        """SimpleGraph constructor."""
+        self._graph_content = {}
