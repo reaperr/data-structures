8dbeaf57ad8e2fa0cb6487ec51f2135ae3427c11
icarrera <irismcarrera@gmail.com>
2016-03-17 13:43:26 -0700

    Created and tested our edges method.

--- src/simple_graph.py
+++ src/simple_graph.py
@@ -7,3 +7,13 @@ class SimpleGraph(object):
     def __init__(self):
         """SimpleGraph constructor."""
         self._graph_content = {}
+
+    def nodes(self):
+        """Return a list of the nodes."""
+        return list(self._graph_content)
+
+    def edges(self):
+        """Return a list of nodes and their edges."""
+        return list(self._graph_content.items())
+
+    
--- src/test_simple_graph.py
+++ src/test_simple_graph.py
@@ -11,6 +11,15 @@ def test_graph():
     test_graph._graph_content = test_dict
     return test_graph
 
+
 def test_init():
     test_graph = SimpleGraph()
     assert test_graph._graph_content == {}
+
+
+def test_nodes(test_graph):
+    assert test_graph.nodes() == [1, 2, 3]
+
+
+def test_edges(test_graph):
+    assert test_graph.edges() == [(1, [2, 3]), (2, [1]), (3, [2])]
